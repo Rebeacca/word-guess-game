05962ba2b67a1c506360116747e295a91f5d644f
Rebeacca Modayil <rebeacca.modayil@temple.edu>
2019-06-05 13:59:25 -0400

    functions for reset and wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,25 @@
 // variables I'll need 
-var character = [
-    "BRIENNEOFTARTH",
+let character = [
+    "ARYA",
     "GENDRY",
-    "EDDARDSTARK",
+    "JOFFREY",
     "YGRITTE",
-    "DAARIONAHARIS",
+    "DROGON",
     "MISSANDEI",
     "STANNIS",
     "RICKON",
-    "THEHOUND",
-    "KHALDROGO"]
-var blanks = 0;
-var blankgood = [];
-var randomChar = "";
-var letters = []
-var wins = 0;
-var losses = 0;
-var wrong = [];
-var guessesRemaining = 6;
+    "TYRION",
+    "VARYS"]
+let blanks = 0;
+let randomChar = "";
+let letters = []
+let blankgood = [];
+let wins = 0;
+let losses = 0;
+let wrong = [];
+let guessesRemaining = 6;
 
-// arrays
+// pt1 
 function Game() {
     randomChar = character[Math.floor(Math.random() * character.length)];
     letters = randomChar.split("");
@@ -31,9 +31,53 @@ function Game() {
 
     document.getElementById("currentcharacter").innerHTML = "  " + blankgood.join("  ");
 
-    console.log(letters);
+    console.log(randomChar);
+    console.log(letters)
     console.log(blanks)
-    console.log(randomChar)
     console.log(blankgood)
+}
+
+// pt2 
+function checkLetters(letter) {
+    var letterInWord = false;
+    for (var i = 0; i < blanks; i++) {
+        if (randomChar[i] == letter) {
+            letterInWord = true;
+        }
+    }
+    if (letterInWord) {
+        for (var i = 0; i < blanks; i++) {
+            if (randomChar[i] == letter) {
+                blankgood[i] = letter;
+            }
+        }
+    }
+    else {
+        wrong.push(letter);
+        guessesRemaining--;
+    }
+    console.log(blankgood);
+}
+// pt3 
+function reset() {
+    guessesRemaining = 6;
+    wrong = [];
+    blankgood = [];
+    Game()
+}
+
+function complete() {
+    console.log("wins:" + wins + "guesses remaining:" + guessesRemaining)
+    if (letters.toString() == blankgood.toString()) {
+        wins++;
+        reset()
+        document.getElementById("wins").innerHTML = " " + wins;
+
+    } else if (guessesRemaining === 0) {
+        reset()
+    }
+
+    document.getElementById("currentcharacter").innerHTML = "  " + blankgood.join(" ");
+    document.getElementById("guessesRemaining").innerHTML = " " + guessesRemaining;
 }
 

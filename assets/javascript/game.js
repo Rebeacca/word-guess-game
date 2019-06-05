50b03896e15f83c5fb6f3143ef32e7bb31f6d9a7
Rebeacca Modayil <rebeacca.modayil@temple.edu>
2019-06-05 13:06:32 -0400

    variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,21 @@
+// variables I'll need 
+var character = [
+    "BRIENNEOFTARTH",
+    "GENDRY",
+    "EDDARDSTARK",
+    "YGRITTE",
+    "DAARIONAHARIS",
+    "MISSANDEI",
+    "STANNIS",
+    "RICKON",
+    "THEHOUND",
+    "KHALDROGO"]
+var blanks = 0;
+var blankgood = [];
+var randomChar = "";
+var letters = []
+var wins = 0;
+var losses = 0;
+var wrong = [];
+var guessesRemaining = 6;
 
